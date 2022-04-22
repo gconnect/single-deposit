33b91b4543cd78be68162d4ad1f50a16c930f7a0
Glory Agatevure <agatevureglory@gmail.com>
2022-04-22 23:06:04 +0100

    created the contract

--- contract.py
+++ contract.py
@@ -1,18 +1,53 @@
 from pyteal import *
 
-on_create = Seq([
-  App.globalPut(Bytes("contract_creator"), Txn.sender()),
-  App.globalPut(Bytes("deposit"), Int(0)),
-  Return(Int(1))
-])
+def approval_program():
+  
+  on_create = Seq([
+    App.globalPut(Bytes("contract_creator"), Txn.sender()),
+    App.globalPut(Bytes("deposit"), Int(0)),
+    Return(Int(1))
+  ])
 
-on_asset_transfer = Seq([
-  InnerTxnBuilder.Begin(),
-  InnerTxnBuilder.SetFields({
-    TxnField.type_enum: TxnType.AssetTransfer,
-    TxnField.asset_receiver: Txn.Sender(),
-    TxnField.asset_amount: Int(1000),
-    TxnField.xfer_asset: Txn.assets[0], # Must be in the assets array sent as part of the application call
-}),
-InnerTxn.Submit(),
-])
+  on_asset_transfer = Seq([
+    Assert(Global.group_size() == Int(1)),
+    Assert(Txn.application_args.length() == Int(3)), 
+    InnerTxnBuilder.Begin(),
+    InnerTxnBuilder.SetFields({
+      TxnField.type_enum: TxnType.AssetTransfer,
+      TxnField.asset_receiver:  Global.current_application_address(),
+      TxnField.asset_amount: Btoi(Txn.application_args[1]),
+      TxnField.xfer_asset: Txn.assets[0],
+      TxnField.fee : Int(0),
+      TxnField.rekey_to: Txn.sender()
+  }),
+  InnerTxn.Submit(),
+  ])
+  
+  program = Cond(
+        [Txn.application_id() == Int(0), on_create],
+        [Txn.on_completion() == OnComplete.NoOp, on_asset_transfer],
+        [
+            Or(
+            Txn.on_completion() == OnComplete.OptIn,
+            Txn.on_completion() == OnComplete.CloseOut,
+            Txn.on_completion() == OnComplete.UpdateApplication,
+            Txn.on_completion() == OnComplete.DeleteApplication,
+            ),
+            Reject(),
+        ],
+    )
+
+  return program
+
+def clear_state_program():
+  
+    return Int(1)
+
+
+with open("approval.teal", "w") as f:
+    compiled = compileTeal(approval_program(), mode=Mode.Application, version=6)
+    f.write(compiled)
+
+with open("clear_state.teal", "w") as f:
+    compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=6)
+    f.write(compiled)
