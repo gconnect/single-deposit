7ce202f32d93cb46050ba0fe336fbef2b8edd86b
Glory Agatevure <agatevureglory@gmail.com>
2022-04-23 00:55:44 +0100

    deplyed teal

--- contract.py
+++ contract.py
@@ -7,25 +7,31 @@ def approval_program():
     Return(Int(1))
   ])
 
-  on_asset_transfer = Seq([
-    Assert(Global.group_size() == Int(1)),
-    Assert(Txn.application_args.length() == Int(3)), 
-    
-    InnerTxnBuilder.Begin(),
-    InnerTxnBuilder.SetFields({
-      TxnField.type_enum: TxnType.AssetTransfer,
-      TxnField.asset_receiver:  Global.current_application_address(),
-      TxnField.asset_amount: Btoi(Txn.application_args[1]),
-      TxnField.xfer_asset: Txn.assets[0],
-      TxnField.fee : Int(0),
-      TxnField.rekey_to: Txn.sender()
-    }),
-    InnerTxn.Submit(),
-    ])
+  def deposit():
+    return Seq([
+        Assert(Global.group_size() == Int(1)),
+        Assert(Txn.application_args.length() == Int(2)), 
+        
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields({
+          TxnField.type_enum: TxnType.Payment,
+          TxnField.sender : Txn.sender(),
+          TxnField.receiver:  Global.current_application_address(),
+          TxnField.amount: Btoi(Txn.application_args[1]),
+          TxnField.fee : Int(0),
+          TxnField.rekey_to: Txn.sender()
+        }),
+        InnerTxnBuilder.Submit(),
+        Approve()
+      ])
+   
+  call = Cond(
+          [Txn.application_args[0] == Bytes("deposit"), deposit()]
+        )
   
   program = Cond(
         [Txn.application_id() == Int(0), on_create],
-        [Txn.on_completion() == OnComplete.NoOp, on_asset_transfer],
+        [Txn.on_completion() == OnComplete.NoOp, call],
         [
             Or(
             Txn.on_completion() == OnComplete.OptIn,
